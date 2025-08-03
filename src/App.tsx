354dd343eccbe098574ca157deaf97ae869c97fb
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-08-03 01:09:52 -0400

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import AdvancedCounter from './components/AdvancedCounter'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <AdvancedCounter/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AdvancedCounter.tsx
@@ -0,0 +1,54 @@
+import {useState, useEffect} from 'react'
+
+export default function AdvancedCounter() {
+    const [count, setCount]=useState<number>(0)
+    const [historyCount, setHistoryCount]=useState<number[]>([0])
+
+    // history tracking
+    useEffect(()=>{
+if (historyCount) {
+    setHistoryCount(prevCount => [...prevCount, count ])
+    return setHistoryCount([count])
+}
+
+    },[count])
+
+// local storage 
+useEffect (()=>{
+    localStorage.setItem("count", JSON.stringify(count)), {
+    }
+}, [count])
+
+
+ useEffect(()=>{
+          const saved = localStorage.getItem("count")
+        const initialValue= saved ? JSON.parse(saved): 0
+        setCount (initialValue)
+    }, [])
+
+    // handler for reset
+const handleClick = () => {
+    setCount(0), 
+    setHistoryCount([0])
+  };
+
+
+    return (
+        <div>
+<h2>Advanced Counter</h2>
+<p>Count: {count}</p>
+<button onClick={()=> setCount(c=> c+ 1)} >Increment Count</button>
+<button onClick={()=> setCount(c=> c- 1)}>Decrement Count</button>
+<button onClick={handleClick}>Reset</button>
+<div>
+<h4>Count History:</h4>
+</div>
+  </div>
+        
+    )
+
+
+
+}
+
+
